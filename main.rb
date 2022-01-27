ffdab6e268a5e8919328ea582f6e13ad1927f50f
ngtspc <cube1090@gmail.com>
2022-01-28 00:39:11 +0200

    Main.rb has been changed

--- main.rb
+++ main.rb
@@ -1,21 +1,6 @@
 # frozen_string_literal: true
-# # frozen_string_literal: true
 
-# require_relative 'library'
+require_relative 'library'
 
-# rating_args = { orders: (FileHandler.new('orders').parse_file),
-#                 books: (FileHandler.new('books').parse_file),
-#                 authors: (FileHandler.new('authors').parse_file),
-#                 clients: (FileHandler.new('clients').parse_file) }
-
-# entities = { books: Book.new(rating_args[:books]),
-#              authors: Author.new(rating_args[:authors]),
-#              clients: Client.new(rating_args[:clients]),
-#              orders: Order.new(rating_args[:orders]),
-#              rating: Rating.new(rating_args[:orders],
-#                                 rating_args[:books],
-#                                 rating_args[:authors],
-#                                 rating_args[:clients]) }
-
-# library = Library.new(entities)
-# library.run
+library = Library.new
+library.run
